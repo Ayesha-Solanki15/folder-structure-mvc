64addf21432aebc12eb4f08c88c3e9ba045ae1e2
Ayesha <ayeshasolanki151@gmail.com>
2025-01-18 18:41:20 +0530

    connecting controller to service layer

--- src/controller/tweetController.js
+++ src/controller/tweetController.js
@@ -1,3 +1,5 @@
+import { createTweet as createTweetService } from "../services/tweetService";
+
 export const getTweets = (req, res) => {
   return res.json({
     message: "Hello from tweets",
@@ -10,8 +12,20 @@ export const getTweetById = (req, res) => {
   });
 };
 
-export const createTweet = (req, res) => {
-  return res.json({
-    message: "Hello from create tweet",
-  });
+export const createTweet = async (req, res) => {
+  try {
+    const tweet = await createTweetService({
+      body: req.body.body,
+    });
+    return res.status(201).json({
+      success: true,
+      data: tweet,
+      message: "Tweet created successfully",
+    });
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
